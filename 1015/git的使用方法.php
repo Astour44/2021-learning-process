3cb0c58ae54025c98df7b50a46dd22c8cf1f38a0
Astour <astour44@gmail.com>
2021-10-15 14:27:26 +0800

    git使用方法

--- 1015/git的使用方法.php
+++ 1015/git的使用方法.php
@@ -2,12 +2,18 @@
 
 /*git用法
 ($ git init)->初始化(將沒上傳給git的資料夾上傳)
-($ git status)
+($ git status)->查看檔案狀態
 ($ git add . 檔案名稱)->個別上傳
 ($ git add .)->全部上傳
-($ git commit -m"這次上傳的名稱")
+($ git commit -m"這次上傳後儲存的名稱")
 ($ git log)->查詢變更的時間
 
-*/
+記得下載gitlens外掛
+回家將git載好後對資料夾做Bash後init
 
+
+
+
+
+*/
 ?>
