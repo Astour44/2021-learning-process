7eda6006e6dacbd5b7d3c9e8eb66dec082f31533
Astour <astour44@gmail.com>
2021-10-15 13:55:25 +0800

    first-time

--- 1015/star.php
+++ 1015/star.php
@@ -2,7 +2,21 @@
 
 
 
-
+$row = 6;
+for ($i = 1; $i <= $row; $i = $i + 1) {
+    for ($j = 1; $j <= $row; $j++) {
+        if ($i == 1 || $i == $row) {
+            echo "*";
+        } else {
+            if ($j == 1 || $j == $row) {
+                echo "*";
+            } else {
+                echo "&ensp;";
+            }
+        }
+    }
+    echo "<br>";
+}
 
 
 
