abcaff28f059bdcd40f55ad2b24dd38d3fdb7b60
Astour <astour44@gmail.com>
2021-10-15 13:57:02 +0800

    first-time

--- 1015/star.php
+++ 1015/star.php
@@ -2,26 +2,6 @@
 
 
 
-$row = 6;
-for ($i = 1; $i <= $row; $i = $i + 1) {
-    for ($j = 1; $j <= $row; $j++) {
-        if ($i == 1 || $i == $row) {
-            echo "*";
-        } else {
-            if ($j == 1 || $j == $row) {
-                echo "*";
-            } else {
-                echo "&ensp;";
-            }
-        }
-    }
-    echo "<br>";
-}
-
-
-
-
-
 
 
 
