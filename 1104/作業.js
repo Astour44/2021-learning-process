5c2a6597d53a9d8e8e2355a4e554a44c378504e1
Astour44 <astour44@gmail.com>
2021-11-04 16:31:09 +0800

    作業JS乘法表

---
+++ 1104/作業.js
@@ -0,0 +1,15 @@
+// for(let i=1;i<10;i++){
+//     for(let a=1;a<10;a++){
+//         console.logA(i*a)
+//     }
+// }
+
+// const form = document.getElementById("myform")
+// const num1 = document.getElementById("num1")
+// const num2 = document.getElementById("num2")
+// const num3 = document.getElementById("num3")
+// form.addEventListener("submit", function (e) {
+//     e.preventDefault();
+//     total = parseInt((num1.value) * parseInt(num2.value))
+//     num3.innerText = "答案是 : " + total;
+})
